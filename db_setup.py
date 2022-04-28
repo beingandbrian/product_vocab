3ba60a04286f271ca24f54c315e29ab2f8c5b4de
beingandbrian <brianslyew@gmail.com>
2022-04-28 12:01:44 -0400

    replace user defined context manager with Session class context manager

--- db_setup.py
+++ db_setup.py
@@ -1,21 +1,7 @@
 # establish db connection like a context manager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
 from model import *
-
-# context manager for session
-@contextmanager
-def session_manager():
-    # instantiating a session from a Session class
-    session = Session()
-    try:
-        yield session
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
 
 engine = create_engine('sqlite:///vocab.db')
 Session = sessionmaker(bind=engine)
